88368906b289936a45f5c0ffa1af5339832aa113
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-26 09:55:56 -0700

    [documentation] add documentation to functions

--- server/src/utilities/database.js
+++ server/src/utilities/database.js
@@ -1,4 +1,4 @@
-const { PrismaClient, Prisma } = require("../generated/prisma");
+const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
 const TABLE_NAMES_ENUM = {
@@ -6,27 +6,55 @@ const TABLE_NAMES_ENUM = {
   COMPANIES: "company",
 };
 
+/**
+ * Format the table name to the prisma model name
+ * @param {*} tableName - name of the table
+ */
 function formatTableName(tableName) {
   return typeof tableName === "string"
     ? prisma[tableName.toLowerCase()]
     : prisma[tableName];
 }
 
+/**
+ * Scan the table named tableName for a specific record statisfying the clauses
+ * @param {*} tableName - name of the table
+ * @param {*} clauses - sql clauses to filter the records
+ * @returns
+ */
 async function scan(tableName, clauses) {
   const model = formatTableName(tableName);
   return await model.findUnique(clauses);
 }
 
+/**
+ * Create a new record in the table named tableName with the creationData
+ * @param {*} tableName - name of the table
+ * @param {*} creationData - data to create the record with
+ */
 async function create(tableName, creationData) {
   const model = formatTableName(tableName);
   return await model.create({ data: creationData });
 }
 
+/**
+ * Get all records in the table named tableName
+ * @param {*} tableName - name of the table
+ */
 async function getAll(tableName) {
   const model = formatTableName(tableName);
   return await model.findMany();
 }
 
+
+/**
+ * Get a page of records in the table named tableName
+ * @param {*} tableName - name of the table
+ * @param {*} currentPageNumber - 0 based
+ * @param {*} pageSize - number of records per page
+ * @param {*} blockSize - number of pages to fetch at a time
+ * @returns
+ */
 async function getPages(tableName, currentPageNumber, pageSize, blockSize) {
   const model = formatTableName(tableName);
   return await model.findMany({
@@ -36,6 +64,10 @@ async function getPages(tableName, currentPageNumber, pageSize, blockSize) {
   });
 }
 
+/**
+ * Get the number of records in the table named tableName
+ * @param {*} tableName - name of the table
+ */
 async function tableCardinality(tableName) {
   const model = formatTableName(tableName);
   return await model.count();
