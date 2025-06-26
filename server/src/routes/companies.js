1a0b06b1233bc908c89cd600a601adf7e9bccc3b
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-25 21:13:52 -0700

    [feature] batched database retrievel for pages

--- server/src/routes/companies.js
+++ server/src/routes/companies.js
@@ -2,16 +2,20 @@ const express = require("express");
 const database = require("../utilities/database");
 const router = express.Router();
 
+// the number of pages
 const BLOCK_SIZE = 4
+// the number of entries per page
 const PAGE_SIZE = 20
 
 router.get("/api/companies", async (req, res) => {
   try {
     const page = parseInt(req.query.page)
-    const pages = []
-    for (let currentPageId = page; currentPageId < page + BLOCK_SIZE; currentPageId++) {
-        const companiesData = await database.getPage(database.TABLE_NAMES_ENUM.COMPANIES, PAGE_SIZE)
-        pages.push({pageNumber: currentPageId, companiesData})
+    const pages = new Array(BLOCK_SIZE)
+    const companiesData = await database.getPages(database.TABLE_NAMES_ENUM.COMPANIES, page, PAGE_SIZE, BLOCK_SIZE)
+    const startIndex = (page*PAGE_SIZE);
+    for (let idx = 0; idx < BLOCK_SIZE; idx++) {
+      const k = startIndex + (idx * PAGE_SIZE)
+      pages[idx] = {pageNumber: page + idx, companiesData: companiesData.slice(k, k + PAGE_SIZE)}
     }
 
     res.status(200).json({
