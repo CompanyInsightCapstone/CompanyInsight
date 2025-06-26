7f7ecd04c3ed22b9421869ccb07845f1e96422ed
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-26 08:55:20 -0700

    [feature] Prev and next page buttons now work within block size scope

--- client/src/api/companies.js
+++ client/src/api/companies.js
@@ -1,2 +1,17 @@
-const companiesM = {};
-export default companiesM;
+import { SERVER_ADDRESS, options, METHOD_ENUM} from "./util";
+
+
+const Companies = {
+    async fetchPage(pageNumber) {
+        const url = `${SERVER_ADDRESS}/api/companies?page=${pageNumber}`
+        const response =  await (
+            await fetch(url, {
+              ...options(METHOD_ENUM.GET),
+              credentials: "include",
+            })
+          ).json();
+        return response;
+    }
+}
+
+export {Companies};
--- client/src/contexts/CompanyListContext.jsx
+++ client/src/contexts/CompanyListContext.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, createContext } from "react";
-import { SERVER_ADDRESS, options, METHOD_ENUM } from "../api/util";
+import { Companies } from "../api/companies";
 
 export const CompanyListContext = createContext();
 
@@ -7,30 +7,21 @@ export default function CompanyListProvider({ children }) {
   const [companies, setCompanies] = useState(new Map());
   const [currentPageNumber, setCurrentPageNumber] = useState(0)
 
-  const updateCompanyList = (newList) => {
-    setCompanies(newList);
+  const updateCompanyList = (newMap) => {
+    setCompanies(newMap);
   };
 
-  const updateCurrentPageNumber = (n) => {
-    setCurrentPageNumber(n)
+  const updateCurrentPageNumber = (v) => {
+    setCurrentPageNumber(newPageNumber => newPageNumber + v)
   }
 
-  const fetchCompanies = async () => {
-    const url = `${SERVER_ADDRESS}/api/companies?page=${currentPageNumber}`
-    const response = await (
-      await fetch(url, {
-        ...options(METHOD_ENUM.GET),
-        credentials: "include",
-      })
-    ).json();
-    const pages = response.pages;
-    pages.forEach(
-        page => companies.set(page.pageNumber, page.companiesData))
-    setCompanies(new Map(companies));
-  };
-
   useEffect(() => {
-    fetchCompanies();
+    async function fetchData() {
+      const data = await Companies.fetchPage(currentPageNumber);
+      data.pages.forEach(page => companies.set(page.pageNumber, page.companiesData))
+      setCompanies(new Map(companies));
+    }
+    fetchData();
   }, []);
 
   return (
