f62ef9587771cb08bc68977a22a3c4777f643945
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-27 00:09:41 -0700

    [feature] add paginated filter including name

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,47 +1,133 @@
+import { useContext, useState } from "react";
+import { CompanyListContext } from "../contexts/CompanyListContext";
+
+const FILTER_FIELDS = {
+  NAME: "name",
+  IPO_DATE: "ipoDate",
+  EXCHANGE: "exchange",
+  ASSET_TYPE: "assetType",
+  STATUS: "status",
+};
+
 export default function Filter() {
+  const { setFilterRequest } = useContext(CompanyListContext);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleClear = (event) => {
+    event.preventDefault();
+    setFilterRequest(null);
+    document.getElementById("filter-form").reset();
+  };
+
+  const isValidFilterRequest = (formData) => {
+    const formEntries = Array.from(formData.entries());
+    const hasValidFilter = formEntries.some(([name, value]) => {
+      if (name === FILTER_FIELDS.NAME && value.trim() !== "") {
+        return true;
+      }
+      if (
+        name !== FILTER_FIELDS.NAME &&
+        name !== FILTER_FIELDS.IPO_DATE &&
+        value !== "all"
+      ) {
+        return true;
+      }
+      if (name === FILTER_FIELDS.IPO_DATE && value !== "") {
+        return true;
+      }
+      return false;
+    });
+    return hasValidFilter;
+  };
+
+  const handleFilterSubmit = (event) => {
+    event.preventDefault();
+    setErrorMessage("");
+    const formData = new FormData(event.target);
+    if (isValidFilterRequest(formData)) {
+      setFilterRequest(formData);
+    } else {
+      setErrorMessage(
+        "Please select at least one filter option before searching.",
+      );
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+    }
+  };
 
   return (
     <section className="filter">
       <div className="filter-controls-container">
-        <div className="filter-controls">
-          <div className="filter-control">
-            <label htmlFor="filter-name">Name</label>
-            <input type="text" id="filter-name" name="filter-name" />
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+        <form id="filter-form" onSubmit={handleFilterSubmit}>
+          <div className="filter-controls">
+            <div className="filter-control">
+              <label htmlFor={FILTER_FIELDS.NAME}>Name</label>
+              <input
+                type="text"
+                id={FILTER_FIELDS.NAME}
+                name={FILTER_FIELDS.NAME}
+              />
+            </div>
+            <div className="filter-control">
+              <label htmlFor={FILTER_FIELDS.IPO_DATE}>IPO Date</label>
+              <select id={FILTER_FIELDS.IPO_DATE} name={FILTER_FIELDS.IPO_DATE}>
+                <option value="">All</option>
+                <option value="earliest">Earliest</option>
+                <option value="latest">Latest</option>
+              </select>
+            </div>
+            <div className="filter-control">
+              <label htmlFor={FILTER_FIELDS.EXCHANGE}>Exchange</label>
+              <select
+                id={FILTER_FIELDS.EXCHANGE}
+                name={FILTER_FIELDS.EXCHANGE}
+                defaultValue="all"
+              >
+                <option value="all">All</option>
+                <option value="NASDAQ">Nasdaq</option>
+                <option value="NYSE">NYSE</option>
+              </select>
+            </div>
+            <div className="filter-control">
+              <label htmlFor={FILTER_FIELDS.ASSET_TYPE}>Asset Type</label>
+              <select
+                id={FILTER_FIELDS.ASSET_TYPE}
+                name={FILTER_FIELDS.ASSET_TYPE}
+                defaultValue="all"
+              >
+                ex
+                <option value="all">All</option>
+                <option value="Stock">Stock</option>
+                <option value="ETF">ETF</option>
+              </select>
+            </div>
+            <div className="filter-control">
+              <label htmlFor={FILTER_FIELDS.STATUS}>Status</label>
+              <select
+                id={FILTER_FIELDS.STATUS}
+                name={FILTER_FIELDS.STATUS}
+                defaultValue="all"
+              >
+                <option value="all">All</option>
+                <option value="Active">Active</option>
+                <option value="Delisted">Delisted</option>
+                <option value="Bankrupt">Bankrupt</option>
+                <option value="Defunct">Defunct</option>
+              </select>
+            </div>
+            <div className="filter-control filter-buttons">
+              <button type="submit" className="filter-button">
+                Apply Filters
+              </button>
+              <button onClick={handleClear} className="clear-button">
+                Clear Filters
+              </button>
+            </div>
           </div>
-          <div className="filter-control">
-            <label htmlFor="filter-ipo-date">IPO Date</label>
-            <select id="filter-ipo-date" name="filter-ipo-date">
-              <option value="earliest">Earliest</option>
-              <option value="latest">Latest</option>
-            </select>
-          </div>
-          <div className="filter-control">
-            <label htmlFor="filter-exchange">Exchange</label>
-            <select id="filter-exchange" name="filter-exchange">
-              <option value="all">All</option>
-              <option value="nasdaq">Nasdaq</option>
-              <option value="nyse">NYSE</option>
-            </select>
-          </div>
-          <div className="filter-control">
-            <label htmlFor="filter-asset-type">Asset Type</label>
-            <select id="filter-asset-type" name="filter-asset-type">
-              <option value="all">All</option>
-              <option value="stock">Stock</option>
-              <option value="etf">ETF</option>
-            </select>
-          </div>
-          <div className="filter-control">
-            <label htmlFor="filter-status">Status</label>
-            <select id="filter-status" name="filter-status">
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="delisted">Delisted</option>
-              <option value="bankrupt">Bankrupt</option>
-              <option value="defunct">Defunct</option>
-            </select>
-          </div>
-        </div>
+        </form>
       </div>
     </section>
   );
