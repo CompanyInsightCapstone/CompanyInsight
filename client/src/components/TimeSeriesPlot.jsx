f462a0ce7923738e5d9c080567fa29f1d7aa487b
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-07-07 12:34:39 -0700

    [refractor] add custom errors to routes to reduce boilerplate catch clauses

---
+++ client/src/components/TimeSeriesPlot.jsx
@@ -0,0 +1,10 @@
+import * as d3 from 'd3';
+
+
+export default function TimeSeriesPlot({timeSeriesData}) {
+    const {data, width, height, xScale, yScale, colorScale, xValue, yValue, colorValue, title, xAxisLabel, yAxisLabel } = timeSeriesData;
+
+    return (
+        <p>placeholder</p>
+    )
+}
