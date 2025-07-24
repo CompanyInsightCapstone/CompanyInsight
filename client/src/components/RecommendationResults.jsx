79f363e2c022077fb9d797fa5188c95db6544935
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-07-24 11:39:47 -0700

    [feature] add tooltip to recommendations page

--- client/src/components/RecommendationResults.jsx
+++ client/src/components/RecommendationResults.jsx
@@ -3,7 +3,7 @@ import { RecommendationContext } from "../contexts/RecommendationContext";
 import CompanyItem from "./CompanyItem";
 import "../styles/SearchResults.css";
 
-export default function RecommendationContextResults() {
+export default function RecommendationResults() {
   const { RecommendationContextData } = useContext(RecommendationContext);
 
   return (
@@ -12,7 +12,14 @@ export default function RecommendationContextResults() {
         {RecommendationContextData.companies &&
           RecommendationContextData.companies.length > 0 && (
             <>
-              <h2>We recommend these companies to you.</h2>
+            <div class="tooltip"><h2>We recommend these companies to you.</h2>
+              <span class="tooltiptext">
+                <p>
+                These companies are similiar to companies from those in your watchlist,
+                if your watchlist is empty, we serve you randomized companies.
+                </p>
+              </span>
+              </div>
               <section className="list-container">
                 {RecommendationContextData.companies.map((result) => {
                   return <CompanyItem key={result.id} company={result} />;
